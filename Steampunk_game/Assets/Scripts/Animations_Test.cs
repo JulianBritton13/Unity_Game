1ebb25fe637210135873b614e8f72aac2b3bd0a6
JulianBritton13 <46837272+JulianBritton13@users.noreply.github.com>
2020-05-13 10:16:40 +0200

    Added test scene and test animation

---
+++ Steampunk_game/Assets/Scripts/Animations_Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations_Test : MonoBehaviour
+{
+    public Animator animator;
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        
+        if(y < 0.25 && y >=0)
+        {
+            y = 0f;
+        }
+        else if(y >= 0.25 && y < 0.75)
+        {
+            y = 0.5f;
+        }
+        else if(y >= 0.75)
+        {
+            y = 1f;
+        }
+        else if(y < 0)
+        {
+            y = -0.5f;
+        }
+        animator.SetFloat("xFloat", x);
+        animator.SetFloat("yFloat", y);
+    }
+}
