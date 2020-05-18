57dc35982a650b7615047e23154f3f9863408d4c
JulianBritton13 <46837272+JulianBritton13@users.noreply.github.com>
2020-05-18 10:26:48 +0200

    Added third person camera
    Added camera add terrain climping for the camera

---
+++ Steampunk_game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float CameraMoveSpeed = 120f;
+    public GameObject CarmeraFollowObj;
+    Vector2 FollowPOS;
+    public float clampAngle = 80f;
+    public float inputSensitivity = 150f;
+    public GameObject CameraObj;
+    public GameObject PlayerObj;
+    public float CamDistanceXToPlayer;
+    public float CamDistanceYToPlayer;
+    public float CamDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    float rotY = 0f;
+    float rotX = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = inputX + mouseX;
+        finalInputZ = inputZ + mouseY;
+
+        rotY += finalInputX * inputSensitivity * Time.deltaTime;
+        rotX += finalInputZ * inputSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+
+
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+
+    void CameraUpdater()
+    {
+        Transform target = CarmeraFollowObj.transform;
+
+
+        float step = CameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
